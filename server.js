a7a820fe563c24c5c256c56f5cd557f89bc703e3
Michael Scott <mscotteagle21@gmail.com>
2024-04-27 13:51:47 -0500

    added express-sequelize-connect for connecting to database and created and verifed dashboard.js route worked

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const session = require('express-session');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3007;
@@ -15,6 +16,9 @@ const sess = {
   secret: 'Super secret secret',
   resave: false,
   saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  })
 };
 
 app.use(session(sess));
