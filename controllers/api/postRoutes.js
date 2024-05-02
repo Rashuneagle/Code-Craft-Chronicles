572dfac57b08a78e9decbff3300291332356fad4
Michael Scott <mscotteagle21@gmail.com>
2024-05-02 07:05:28 -0500

    fixed routes for new posts, created data relationships, edited newPost and dashboard handlebars file

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id // Assign the logged-in user's ID to the new post
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+// DELETE route to delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id, // Ensure the post belongs to the logged-in user
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
